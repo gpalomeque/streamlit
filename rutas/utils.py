e0ae6630b14dc969773a9ef50f7413525984d2c5
gpalomeque <gpalomeque@gmail.com>
2024-03-22 17:33:16 -0600

    Upd utils.py
    agregar vsicurl

--- rutas/utils.py
+++ rutas/utils.py
@@ -3,9 +3,13 @@ import pandas as pd
 
 class Rutas:
     def __init__(self, path_rutas, path_rutas_paradas):
-        self.df_rutas = gpd.read_file(path_rutas)
-        self.df_rutas_paradas = gpd.read_file(path_rutas_paradas)
+        #self.df_rutas = gpd.read_file(path_rutas)
+        #self.df_rutas_paradas = gpd.read_file(path_rutas_paradas)
         self.df_base = pd.read_csv("data/ubicaciones_base.csv")
+        self.df_rutas = gpd.read_file('/vsicurl/https://github.com/gpalomeque/streamlit/blob/main/rutas/data/CConcesionado_Rutas.shp')
+        self.df_rutas_paradas = gpd.read_file('/vsicurl/https://github.com/gpalomeque/streamlit/blob/main/rutas/data/CConcesionado_Paradas.shp')
+        #self.df_base = pd.read_csv(('/vsicurl/https://github.com/gpalomeque/streamlit/blob/main/rutas/data/CConcesionado_Rutas.shp')
+
     
     def get_rutas(self, claves):
         new_claves = [item for item in claves if item is not None]
